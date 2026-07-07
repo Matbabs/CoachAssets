452361d9d418772d0151b57f08445244fa2c63ac
Matisse BABONNEAU <matisse.babonneau.ext@veolia.com>
2026-07-07 11:11:18 +0200

    skate sessions

--- sessions/genfiles.sh
+++ sessions/genfiles.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <sport_path> <cycle_id>"
+if [ "$#" -lt 3 ] || [ "$#" -gt 4 ]; then
+    echo "Usage: $0 <sport_path> <cycle_id> <end_week> [start_week (default 1)]"
     exit 1
 fi
 
 sport="$1"
 cycle="$2"
+end_week="$3"
+start_week="${4:-1}"
+
+if [ "$start_week" -gt "$end_week" ]; then
+    temp="$start_week"
+    start_week="$end_week"
+    end_week="$temp"
+fi
 
 mkdir -p "$sport" || { echo "Error: Unable to create directory $sport"; exit 1; }
 
-for week in {1..4}; do
+for week in $(seq "$start_week" "$end_week"); do
     jsonFilename="${sport}/c${cycle}.s${week}.json"
     for day in {1..3}; do
         htmlFilename="${sport}/c${cycle}.s${week}.d${day}.html"
